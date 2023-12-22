e841f2d48c373e62259353f600fc7033333241de
lakpa-tamang9 <Ld.tamang25@gmail.com>
2023-12-22 13:32:27 +1100

    Fix colon missing and style changes

--- dataset_utils/ood_loader.py
+++ dataset_utils/ood_loader.py
@@ -5,7 +5,7 @@ import dataset_utils.svhn_loader as svhn
 
 
 def load_ood_data(dataset, batch_size, mean, std):
-    if dataset == "textures"
+    if dataset == "textures":
         ood_data = ImageFolder(
             root="../data/dtd/images",
             transform=T.Compose(
@@ -14,11 +14,14 @@ def load_ood_data(dataset, batch_size, mean, std):
         )
         num_workers = 4
         print("\n\nTexture Detection")
+
     elif dataset == "svhn":
         ood_data = svhn.SVHN(
             root="../data/svhn/",
             split="test",
-            transform=T.Compose([T.ToTensor(), T.Normalize(mean, std)]),  # T.Resize(32),
+            transform=T.Compose(
+                [T.ToTensor(), T.Normalize(mean, std)]
+            ),  # T.Resize(32),
             download=False,
         )
         num_workers = 2
@@ -33,6 +36,7 @@ def load_ood_data(dataset, batch_size, mean, std):
         )
         num_workers = 2
         print("\n\nPlaces365 Detection")
+
     elif dataset == "lsunc":
         ood_data = ImageFolder(
             root="../data/LSUN_C",
@@ -40,6 +44,7 @@ def load_ood_data(dataset, batch_size, mean, std):
         )
         num_workers = 1
         print("\n\nLSUN_C Detection")
+
     elif dataset == "lsunr":
         ood_data = ImageFolder(
             root="../data/LSUN_resize",
@@ -47,15 +52,19 @@ def load_ood_data(dataset, batch_size, mean, std):
         )
         num_workers = 1
         print("\n\nLSUN_Resize Detection")
+
     elif dataset == "isun":
-        # /////////////// iSUN ///////////////
         ood_data = ImageFolder(
             root="../data/iSUN",
             transform=T.Compose([T.ToTensor(), T.Normalize(mean, std)]),
         )
         num_workers = 1
-        
+
     ood_loader = torch.utils.data.DataLoader(
-        ood_data, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True
+        ood_data,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True,
     )
     return ood_loader
