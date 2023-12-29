8dd8941aecff0a28c86fb46944de8d818261dd7c
lakpa-tamang9 <ld.tamang25@gmail.com>
2023-12-29 18:34:04 +1100

    fix: Fix the loss nan

--- loss/loss.py
+++ loss/loss.py
@@ -9,20 +9,23 @@ class MarginLoss(nn.Module):
         self.class_weights = weights
         self.margin = 0.0
 
-    def forward(self, inputs, targets, ood_class_idx):
+    def forward(self, inputs, targets, ood_class_idx, test=False):
         if self.class_weights is not None:
             class_weights = torch.FloatTensor(self.class_weights).to(inputs.device)
         else:
             class_weights = None
         loss = F.cross_entropy(inputs, targets, weight=class_weights)
-        ood_mask = targets == ood_class_idx
-        id_mask = targets != ood_class_idx
+        if not test:
+            ood_mask = targets == ood_class_idx
+            id_mask = targets != ood_class_idx
 
-        ood_score = inputs[ood_mask][:, ood_class_idx]
-        best_id_score, _ = torch.max(inputs[id_mask][:, :ood_class_idx], dim=1)
-        best_id_score_mean = torch.mean(best_id_score)
-        margin_loss = torch.mean(
-            torch.clamp(self.margin - (best_id_score_mean - ood_score), min=0.0)
-        )
-        final_loss = margin_loss + loss
-        return final_loss
+            ood_score = inputs[ood_mask][:, ood_class_idx]
+            best_id_score, _ = torch.max(inputs[id_mask][:, :ood_class_idx], dim=1)
+            best_id_score_mean = torch.mean(best_id_score)
+            margin_loss = torch.mean(
+                torch.clamp(self.margin - (best_id_score_mean - ood_score), min=0.0)
+            )
+            final_loss = margin_loss + loss
+            return final_loss
+        else:
+            return loss
