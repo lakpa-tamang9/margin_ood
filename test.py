71c71c19cc769e3b72cdebb1cd433c5ac78aa5d6
lakpa-tamang9 <ld.tamang25@gmail.com>
2023-12-31 17:20:41 +1100

    feat: integrate accuracy calculation into score method

--- test.py
+++ test.py
@@ -72,55 +72,65 @@ test_loader = torch.utils.data.DataLoader(
 )
 
 texture_loader = torch.utils.data.DataLoader(
-    texture_data, batch_size=test_bs, shuffle=True, num_workers=4, pin_memory=False
+    texture_data, batch_size=test_bs, shuffle=True, pin_memory=False
 )
 svhn_loader = torch.utils.data.DataLoader(
-    svhn_data, batch_size=test_bs, shuffle=True, num_workers=4, pin_memory=False
+    svhn_data, batch_size=test_bs, shuffle=True, pin_memory=False
 )
 places365_loader = torch.utils.data.DataLoader(
     places365_data,
     batch_size=test_bs,
     shuffle=True,
-    num_workers=4,
     pin_memory=False,
 )
 lsunc_loader = torch.utils.data.DataLoader(
-    lsunc_data, batch_size=test_bs, shuffle=True, num_workers=4, pin_memory=False
+    lsunc_data, batch_size=test_bs, shuffle=True, pin_memory=False
 )
 lsunr_loader = torch.utils.data.DataLoader(
-    lsunr_data, batch_size=test_bs, shuffle=True, num_workers=4, pin_memory=False
+    lsunr_data, batch_size=test_bs, shuffle=True, pin_memory=False
 )
 isun_loader = torch.utils.data.DataLoader(
-    isun_data, batch_size=test_bs, shuffle=True, num_workers=4, pin_memory=False
-)
-cifar_loader = torch.utils.data.DataLoader(
-    test_data, batch_size=test_bs, shuffle=True, num_workers=4, pin_memory=False
+    isun_data, batch_size=test_bs, shuffle=True, pin_memory=False
 )
 
 ood_num_examples = len(test_data) // 5
-expected_ap = ood_num_examples / (ood_num_examples + len(test_data))
+print(f"ood num examples is {ood_num_examples}")
 concat = lambda x: np.concatenate(x, axis=0)
 to_np = lambda x: x.data.cpu().numpy()
 
 
-def get_ood_scores(loader, in_dist=False):
+def get_scores(loader, calc_id_acc=False):
     _score = []
+    acc = []
+    correct = 0
+    total = 0
     net.eval()
     with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(loader):
-            if batch_idx >= ood_num_examples // test_bs and in_dist is False:
-                break
-            data, target = data.to(device), target.to(device)
+        for _, (data, targets) in enumerate(loader):
+            # if batch_idx >= ood_num_examples // test_bs and in_dist is False:
+            #     break
+            data, targets = data.to(device), targets.to(device)
             output = net(data)
-            # smax = to_np(F.softmax(output, dim=1))
-            smax = to_np(output)
-            _score.append(-np.max(smax, axis=1))
-    if in_dist:
-        return concat(
-            _score
-        ).copy()  # , concat(_right_score).copy(), concat(_wrong_score).copy()
+
+            if calc_id_acc:
+                # Calculate accuracy
+                _, predicted = output.max(1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).sum().item()
+                acc.append(100.0 * correct / total)
+
+            smax = to_np(F.softmax(output, dim=1))
+            # smax = to_np(output)
+            max_val = -np.max(smax, axis=1)
+            _score.append(max_val)
+    if calc_id_acc:
+        mean_acc = np.mean(acc)
+    ood_score = concat(_score)
+
+    if calc_id_acc:
+        return ood_score.copy(), mean_acc
     else:
-        return concat(_score)[:ood_num_examples].copy()
+        return ood_score.copy()
 
 
 def calc_accuracy(X, true_labels):
@@ -134,7 +144,7 @@ def get_results(ood_loader, in_score, num_to_avg=num_to_avg):
     net.eval()
     aurocs, auprs, fprs = [], [], []
     for _ in range(num_to_avg):
-        out_score = get_ood_scores(ood_loader)
+        out_score = get_scores(ood_loader)
         if out_as_pos:  # OE's defines out samples as positive
             measures = get_measures(out_score, in_score)
         else:
@@ -146,26 +156,6 @@ def get_results(ood_loader, in_score, num_to_avg=num_to_avg):
     aupr = np.mean(auprs)
     fpr = np.mean(fprs)
     return auroc, aupr, fpr
-
-
-def get_id_acc(loader):
-    net.eval()
-    X = []
-    y = []
-    all_accuracy = []
-    with torch.no_grad():
-        for _, (inputs, targets) in enumerate(loader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            X += outputs.cpu().detach().tolist()
-            y += targets.cpu().tolist()
-        X = np.asarray(X)
-        y = np.asarray(y)
-
-        accuracy = calc_accuracy(X, y)
-        all_accuracy += [accuracy]
-    mean_acc = np.mean(all_accuracy)
-    return mean_acc
 
 
 # Restore model
@@ -181,7 +171,7 @@ ood_loaders = {
     "places_365": places365_loader,
 }
 metrics = []
-for i in range(6):
+for i in range(5, 6):
     margin = i / 10
     model_path = (
         f"checkpoint/{dataset}/{learning_model}/32imgnet_oe_margin_{margin}.pth"
@@ -192,10 +182,10 @@ for i in range(6):
     )
     net.to(device)
     net.eval()
-    in_score = get_ood_scores(test_loader, in_dist=True)
-
-    id_accuracy = get_id_acc(cifar_loader)
-    print(f"The id accuracy is {id_accuracy} %")
+    in_score, accuracy = get_scores(test_loader, calc_id_acc=True)
+    print(accuracy)
+    # id_accuracy = get_id_acc(cifar_loader)
+    # print(f"The id accuracy is {id_accuracy} %")
     for ood_name, ood_loader in ood_loaders.items():
         auroc, aupr, fpr = get_results(ood_loader, in_score)
         print(f"Margin {margin} with {ood_name} ood, results : {[auroc, aupr, fpr]}")
