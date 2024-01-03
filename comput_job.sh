327c968271a23b18877da053800fabb72e52d409
lakpa-tamang9 <Ld.tamang25@gmail.com>
2024-01-03 11:09:11 +1100

    feat: update the log directory

--- comput_job.sh
+++ comput_job.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:4           #Assign GPUs
 #SBATCH --time=5:00:00        #Time Limit
 #SBATCH --mem=40536
-#SBATCH --output=oe_hendrycks_etal.log #Standard output and error log
+#SBATCH --output=/home/s223127906/devs/margin_ood/oe_hendrycks_etal.log #Standard output and error log
 #SBATCH --mail-user=s223127906@deakin.edu.au
 #SBATCH --mail-type=ALL
  
-python /Users/lakpa/devs/deakin/research/development/margin_ood/train_with_outlier_loader.py
+python /Users/lakpa/devs/deakin/research/development/margin_ood/train_with_outlier_loader.py 
