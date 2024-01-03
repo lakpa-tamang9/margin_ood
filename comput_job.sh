30c092d2d4052a55e7f7d4d13809ef731de6432a
lakpa-tamang9 <ld.tamang25@gmail.com>
2024-01-03 16:53:47 +1100

    update slurm cmds

--- comput_job.sh
+++ comput_job.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
  
-#SBATCH --job-name=gpu_network #Job name
-#SBATCH --partition=dgx        #Run on a single gpu
-#SBATCH --gres=gpu:4           #Assign GPUs
-#SBATCH --time=5:00:00        #Time Limit
+#SBATCH --job-name=oodtest #Job name
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=gpu-dev-1080
+#SBATCH --nodelist=thorin-[1-4]
+#SBATCH --gres=gpu:2           #Assign GPUs
+#SBATCH --time=24:00:00        #Time Limit
 #SBATCH --mem=40536
-#SBATCH --output=oe_hendrycks_etal.log #Standard output and error log
-#SBATCH --mail-user=tboonesifuentes@deakin.edu.au
+#SBATCH --output=./oe_hendrycks_etal_%j.log #Standard output and error log
+#SBATCH --mail-user=s223127906@deakin.edu.au
 #SBATCH --mail-type=ALL
- 
-python /Users/lakpa/devs/deakin/research/development/margin_ood/train_with_outlier_loader.py
+
+source /home/s223127906/.conda/etc/profile.d/conda.sh
+conda activate /home/s223127906/.conda/envs/cssr
+
+cd /home/s223127906/deakin_devs/margin_ood
+srun python train_with_outlier_loader.py
