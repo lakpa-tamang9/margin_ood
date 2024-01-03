01472d0f8f6bbdd1585ac75638a60e8ecbd19bc7
lakpa-tamang9 <Ld.tamang25@gmail.com>
2024-01-03 11:10:23 +1100

    chore: remove memory var

--- comput_job.sh
+++ comput_job.sh
@@ -4,7 +4,6 @@
 #SBATCH --partition=dgx        #Run on a single gpu
 #SBATCH --gres=gpu:4           #Assign GPUs
 #SBATCH --time=5:00:00        #Time Limit
-#SBATCH --mem=40536
 #SBATCH --output=/home/s223127906/devs/margin_ood/oe_hendrycks_etal.log #Standard output and error log
 #SBATCH --mail-user=s223127906@deakin.edu.au
 #SBATCH --mail-type=ALL
