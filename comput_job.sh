cdfc09d9d5aea0cd1f392eafaa4d779afe6dffaa
lakpa-tamang9 <ld.tamang25@gmail.com>
2024-03-18 19:21:44 +1100

    sss

--- comput_job.sh
+++ comput_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 #SBATCH --job-name=macs_ood #Job name
-#SBATCH --partition=dgx
+#SBATCH --partition=hpc-dgx-b-1
 #SBATCH --gres=gpu:2           #Assign GPUs
 #SBATCH --time=24:00:00        #Time Limit
 #SBATCH --mem=40536
