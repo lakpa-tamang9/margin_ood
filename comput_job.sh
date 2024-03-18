9a6d93ad999769cb205b234a5bc77b7e103ecbdb
lakpa-tamang9 <ld.tamang25@gmail.com>
2024-03-18 19:23:28 +1100

    sss

--- comput_job.sh
+++ comput_job.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
  
 #SBATCH --job-name=macs_ood #Job name
-#SBATCH --partition=hpc-dgx-b-1
-#SBATCH --gres=gpu:2           #Assign GPUs
+#SBATCH --partition=dgx
+#SBATCH --gres=gpu:1           #Assign GPUs
 #SBATCH --time=24:00:00        #Time Limit
-#SBATCH --mem=40536
 #SBATCH --output=./MaCS_%x_%j.log 
 #SBATCH --error=./error_%x_%j.log
 #SBATCH --mail-user=s223127906@deakin.edu.au
