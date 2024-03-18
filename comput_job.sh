c16550d0f77b2f99b19ae4e87c6f9019629376ff
lakpa-tamang9 <ld.tamang25@gmail.com>
2024-03-18 19:20:14 +1100

    sss

--- comput_job.sh
+++ comput_job.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
  
-#SBATCH --job-name=oe_ood #Job name
-#SBATCH --ntasks-per-node=1
-#SBATCH --partition=gpu-dev-1080
+#SBATCH --job-name=macs_ood #Job name
+#SBATCH --partition=dgx
 #SBATCH --gres=gpu:2           #Assign GPUs
 #SBATCH --time=24:00:00        #Time Limit
 #SBATCH --mem=40536
-#SBATCH --output=./MaPS_%x_%j.log 
+#SBATCH --output=./MaCS_%x_%j.log 
 #SBATCH --error=./error_%x_%j.log
 #SBATCH --mail-user=s223127906@deakin.edu.au
 #SBATCH --mail-type=ALL
 
-module load cuda/11.4
-
-source /home/s223127906/.conda/etc/profile.d/conda.sh
-conda activate /home/s223127906/.conda/envs/cssr
-
-module load python
-
-cd /home/s223127906/deakin_devs/margin_ood
-srun python mlm_none_train_oe_margin_finetune.py -d cifar10 --detectors msp
+python /home/s223127906/devs/margin_ood/eccv24_final.py
