de467b1e626de575d1029677fa6a0f8fcb61913c
lakpa-tamang9 <ld.tamang25@gmail.com>
2024-03-18 19:08:02 +1100

    test

--- gputest.py
+++ gputest.py
@@ -149,52 +149,47 @@ elif args.dataset == "tinyimagenet":
     num_classes = 200
 
 
-# calib_indicator = ""
-# if args.calibration:
-#     train_data_in, val_data = validation_split(train_data_in, val_share=0.1)
-#     calib_indicator = "_calib"
-
-# if args.outlier_name == "imgnet32":
-#     ood_data = ImageNetDownSample(
-#         root="./data/ImageNet32",
-#         transform=trn.Compose(
-#             [
-#                 trn.ToTensor(),
-#                 trn.ToPILImage(),
-#                 trn.RandomCrop(32, padding=4),
-#                 trn.RandomHorizontalFlip(),
-#                 trn.ToTensor(),
-#                 trn.Normalize(mean, std),
-#             ]
-#         ),
-#     )
-# elif args.outlier_name == "300k":
-#     ood_data = RandomImages(
-#         transform=trn.Compose(
-#             [
-#                 trn.ToTensor(),
-#                 trn.ToPILImage(),
-#                 trn.RandomCrop(32, padding=4),
-#                 trn.RandomHorizontalFlip(),
-#                 trn.ToTensor(),
-#                 trn.Normalize(mean, std),
-#             ]
-#         ),
-#         data_num=args.data_num,
-#     )
-# elif args.outlier_name == "tinyimagenet":
-#     ood_data = dset.ImageFolder(
-#         root="DOE/data/tiny-imagenet-200/train",
-#         transform=trn.Compose(
-#             [
-#                 trn.Resize(32),
-#                 trn.RandomCrop(32, padding=4),
-#                 trn.RandomHorizontalFlip(),
-#                 trn.ToTensor(),
-#                 trn.Normalize(mean, std),
-#             ]
-#         ),
-#     )
+if args.outlier_name == "imgnet32":
+    ood_data = ImageNetDownSample(
+        root="./data/ImageNet32",
+        transform=trn.Compose(
+            [
+                trn.ToTensor(),
+                trn.ToPILImage(),
+                trn.RandomCrop(32, padding=4),
+                trn.RandomHorizontalFlip(),
+                trn.ToTensor(),
+                trn.Normalize(mean, std),
+            ]
+        ),
+    )
+elif args.outlier_name == "300k":
+    ood_data = RandomImages(
+        transform=trn.Compose(
+            [
+                trn.ToTensor(),
+                trn.ToPILImage(),
+                trn.RandomCrop(32, padding=4),
+                trn.RandomHorizontalFlip(),
+                trn.ToTensor(),
+                trn.Normalize(mean, std),
+            ]
+        ),
+        data_num=args.data_num,
+    )
+elif args.outlier_name == "tinyimagenet":
+    ood_data = dset.ImageFolder(
+        root="DOE/data/tiny-imagenet-200/train",
+        transform=trn.Compose(
+            [
+                trn.Resize(32),
+                trn.RandomCrop(32, padding=4),
+                trn.RandomHorizontalFlip(),
+                trn.ToTensor(),
+                trn.Normalize(mean, std),
+            ]
+        ),
+    )
 
 # train_loader_in = torch.utils.data.DataLoader(
 #     train_data_in,
